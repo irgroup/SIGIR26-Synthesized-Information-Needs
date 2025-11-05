de690b7eb53c97cff4d9aed0512ebdbe80f4a480
jueri <jueri.keller@th-koeln.de>
2025-11-05 13:04:40 +0000

    update exp2

--- scripts/exp2.1-llm-judge.py
+++ scripts/exp2.1-llm-judge.py
@@ -3,74 +3,53 @@
 Experiment 2.1: Judge relevance of an existing test collection using the title of the topic only
 
 Example usage:
-    python scripts/exp2.1-llm-judge.py --model Qwen/Qwen3-30B-A3B-Instruct-2507-FP8 --k 50 --gpus 1
+    python scripts/exp2.1-llm-judge.py --model qwen3-14B-MT100_no_think --k 1000 --connection http://localhost:6544/v1
 
 """
 import click
 import time
 import os
-import torch
-from langchain.chat_models import init_chat_model
 from topic_gen.generate import Generator
 from topic_gen.models import MTO_responds
-from langchain_community.llms import VLLM
-from src.data import MODELS_DIR, ird_qrels_parser, DATA_DIR_PROCESSED, DATA_DIR_RAW
+from src.data import ird_qrels_parser, DATA_DIR_PROCESSED, DATA_DIR_RAW
 from topic_gen import logger
-from langchain_openai import ChatOpenAI
+
+from src.config import get_llm
 
 logger.setLevel("DEBUG")
 
 
 @click.command()
-@click.option("--model", help="The model to use.")
+@click.option("--model", help="The model to use. This must be registered in src/config.py", required=True, type=str)
 @click.option("--k", help="Generate results only for the first k samples.", default=None, type=int)
+@click.option("--s", help="Generate qrels only for the qrels used in the original paper.", is_flag=True, default=False)
 @click.option("--max_concurrency", help="Maximum number of concurrent requests.", default=50, type=int)
+@click.option("--connection", help="The connection string for the LLM.", default="0", type=str)
 @click.option("--gpus", help="CUDA_VISIBLE_DEVICES", default="0", type=str)
-def main(model, k, max_concurrency, gpus):
-    logger.info(f"Cuda: {torch.cuda.is_available()}")
-    os.environ["CUDA_VISIBLE_DEVICES"] = gpus
-    os.environ["VLLM_WORKER_MULTIPROC_METHOD"] = "spawn"
-    logger.info(f"Number of GPUs: {torch.cuda.device_count()}")
-
-    # Setup the model
-    llm = ChatOpenAI(
-        openai_api_base="http://localhost:6543/v1",
-        openai_api_key="not-needed",
-        model_name=model
-    )
-    # llm = VLLM(model=model,
-    #            download_dir=MODELS_DIR,
-    #            temperature=0.7,
-    #            top_p=0.8,
-    #            top_k=20,
-    #            min_p=0,
-    #            vllm_kwargs={
-    #                "max_model_len": 32768,
-    #                "max_num_batched_tokens": 32768,
-    #                "gpu_memory_utilization": 0.7
-    #            }
-    #            )
+def main(model, k, s, max_concurrency, connection, gpus):
+    # Get LLM
+    llm = get_llm(model, connection=connection, gpus=gpus)
 
     # Load data
     documents, titles, _, _, qrels = ird_qrels_parser.prepare_qrels(
-        "disks45/nocr/trec-robust-2004", k=k)
+        "disks45/nocr/trec-robust-2004", k=k, s=s)
 
     # Setup generator
-    prompt_name = "robust-DNA-zero-shot-no-title-description"
-    generator = Generator(llm=llm, output_class=MTO_responds,
-                          prompts_dir=DATA_DIR_RAW / "prompts",
-                          prompt_name=prompt_name)
-
-    # Generate judgments
-    start_time = time.time()
+    prompt = "robust-DNA-zero-shot-masked-description-narrative"
+    prompt_file = prompt + ".yaml"
     config = {"return_exceptions": True}
     if max_concurrency is not None:
         config["max_concurrency"] = max_concurrency
+    generator = Generator(llm=llm, output_class=MTO_responds,
+                          prompt=DATA_DIR_RAW / "prompts" / prompt_file,
+                          config=config)
+
+    # Generate judgments
+    start_time = time.time()
 
     res = generator.generate(
         document=documents,
         query=titles,
-        config=config
     )
     end_time = time.time()
     logger.info(f"Execution time: {end_time - start_time} seconds")
@@ -87,12 +66,15 @@ def main(model, k, max_concurrency, gpus):
     qrels["relevance"] = llm_judgments
 
     # format filename
-    output_file = f"qrels-robust-{model.replace('/', '-')}-{prompt_name}"
+    output_file = f"qrels-robust_{model.replace('/', '-')}_{prompt}_topics-trec-title"
     if k is not None:
-        output_file += f"-k{k}"
+        output_file += f"_k{k}"
+    if s:
+        output_file += f"_s"
+
     output_file += ".csv.gz"
 
-    qrels.to_csv(os.path.join(DATA_DIR_PROCESSED,
+    qrels.to_csv(os.path.join(DATA_DIR_PROCESSED, "exp2",
                  output_file), sep=" ", index=False, header=False, compression="gzip")
 
 
