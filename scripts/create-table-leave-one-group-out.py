d73d38aa6642e8ca74f5513049fbd224ba14aab0
Maik Fröbe <maik.froebe@uni-jena.de>
2026-01-20 22:19:03 +0100

    mf

--- scripts/create-table-leave-one-group-out.py
+++ scripts/create-table-leave-one-group-out.py
@@ -7,25 +7,25 @@ from pathlib import Path
 from statistics import mean
 
 def all_logo_tests(dataset):
-    for qrel_file in tqdm(glob(f'../data/interim/{dataset}/qrels-topics-generated-title/**/qrels.csv.gz'), dataset):
+#    for qrel_file in tqdm(glob(f'../data/interim/{dataset}/qrels-topics-generated-title/**/qrels.csv.gz'), dataset):
+#        target_file = Path(f'../data/interim/{dataset}/qrels-analyzed') / Path(qrel_file).parent.name / "top-10-logo" / "reliability-test-results.json.gz"
+#        if not target_file.is_file():
+#            write_job_yaml(dataset, Path(qrel_file).parent.name, "qrels-topics-generated-title")
+#
+#    for qrel_file in tqdm(glob(f'../data/interim/{dataset}/qrels-topics-generated-full/**/qrels.csv.gz'), dataset):
+#        target_file = Path(f'../data/interim/{dataset}/qrels-analyzed') / Path(qrel_file).parent.name / "top-10-logo" / "reliability-test-results.json.gz"
+#        if not target_file.is_file():
+#            write_job_yaml(dataset, Path(qrel_file).parent.name, "qrels-topics-generated-full")
+#
+    for qrel_file in tqdm(glob(f'../data/interim/{dataset}/qrels-topics-generated-title-description/**/qrels.csv.gz'), dataset):
         target_file = Path(f'../data/interim/{dataset}/qrels-analyzed') / Path(qrel_file).parent.name / "top-10-logo" / "reliability-test-results.json.gz"
         if not target_file.is_file():
-            write_job_yaml(dataset, Path(qrel_file).parent.name, "qrels-topics-generated-title")
+            write_job_yaml(dataset, Path(qrel_file).parent.name, "qrels-topics-generated-title-description")
 
-    for qrel_file in tqdm(glob(f'../data/interim/{dataset}/qrels-topics-generated-full/**/qrels.csv.gz'), dataset):
+    for qrel_file in tqdm(glob(f'../data/interim/{dataset}/qrels-topics-generated-title-narrative/**/qrels.csv.gz'), dataset):
         target_file = Path(f'../data/interim/{dataset}/qrels-analyzed') / Path(qrel_file).parent.name / "top-10-logo" / "reliability-test-results.json.gz"
         if not target_file.is_file():
-            write_job_yaml(dataset, Path(qrel_file).parent.name, "qrels-topics-generated-full")
-
-    for qrel_file in tqdm(glob(f'../data/interim/{dataset}/qrels-topics-generated-description/**/qrels.csv.gz'), dataset):
-        target_file = Path(f'../data/interim/{dataset}/qrels-analyzed') / Path(qrel_file).parent.name / "top-10-logo" / "reliability-test-results.json.gz"
-        if not target_file.is_file():
-            write_job_yaml(dataset, Path(qrel_file).parent.name, "qrels-topics-generated-description")
-
-    for qrel_file in tqdm(glob(f'../data/interim/{dataset}/qrels-topics-generated-narrative/**/qrels.csv.gz'), dataset):
-        target_file = Path(f'../data/interim/{dataset}/qrels-analyzed') / Path(qrel_file).parent.name / "top-10-logo" / "reliability-test-results.json.gz"
-        if not target_file.is_file():
-            write_job_yaml(dataset, Path(qrel_file).parent.name, "qrels-topics-generated-narrative")
+            write_job_yaml(dataset, Path(qrel_file).parent.name, "qrels-topics-generated-title-narrative")
 
 
 def write_job_yaml(dataset, qrels_file, directory):
@@ -98,57 +98,72 @@ def run_all_logo_tests():
         all_logo_tests(ds)
 
 def print_results():
-    ALLOWED_TOPIC_FORMATS = set()
-    for qrel_file in glob(f'../data/interim/dl19/qrels-topics-generated-full/**/qrels.csv.gz'):
-        metadata_file = Path(qrel_file).parent / "metadata.json"
-        metadata = json.loads(metadata_file.read_text())
-        ALLOWED_TOPIC_FORMATS.add(metadata["topics"]["prompt"])
-    print(ALLOWED_TOPIC_FORMATS)
+    ret = {}
+    for dataset in ["dl19", "dl20"]:
 
-    qrel_files = {"full": [], "title": []}
+#        qrel_files = {"full": [], "title": [], "title-description": [], "title-narrative": []}
+        qrel_files = {"title-description": [], "title-narrative": []}
 
-    for t in qrel_files.keys():
-        for qrel_file in glob(f'../data/interim/dl19/qrels-topics-generated-{t}/**/qrels.csv.gz'):
-            metadata_file = Path(qrel_file).parent / "metadata.json"
-            metadata = json.loads(metadata_file.read_text())
+        for t in qrel_files.keys():
+            for qrel_file in glob(f'../data/interim/{dataset}/qrels-topics-generated-{t}/**/qrels.csv.gz'):
+                metadata_file = Path(qrel_file).parent / "metadata.json"
+                metadata = json.loads(metadata_file.read_text())
 
-            if metadata["topics"]["prompt"] in ALLOWED_TOPIC_FORMATS:
+                #if metadata["topics"]["prompt"] in ALLOWED_TOPIC_FORMATS:
                 qrel_files[t].append(Path(qrel_file).parent.name)
 
 
-    print({k: len(v) for k, v in qrel_files.items()})
+        print({k: len(v) for k, v in qrel_files.items()})
 
-    def form(corr, measure):
-        return f"{mean(corr_to_measure_to_vals[corr][measure]):.3f}"
+        def form(corr, measure):
+            return f"{mean(corr_to_measure_to_vals[corr][measure]):.3f}"
 
-    skipped = 0
+        skipped = 0
 
-    for t in qrel_files.keys():
-        corr_to_measure_to_vals = {"spearman": {"nDCG@10": [], "nDCG@20": [], "nDCG": []}, "tauap_b": {"nDCG@10": [], "nDCG@20": [], "nDCG": []}}
-        for corr in corr_to_measure_to_vals.keys():
-            for qrel_file in qrel_files[t]:
-                target_file = Path(f"../data/interim/dl19/qrels-analyzed/{qrel_file}/top-10-logo/reliability-test-results.json.gz")
-                if not target_file.exists():
-                    skipped += 1
-                    continue
-                with gzip.open(target_file, "rt") as f:
-                    i = json.loads(f.read())
-                    measures = {"nDCG@10": [], "nDCG@20": [], "nDCG": []}
-                    for r in i["system_ranking_evaluation"]:
+        for t in qrel_files.keys():
+            corr_to_measure_to_vals = {"spearman": {"nDCG@10": [], "nDCG@20": [], "nDCG": []}, "tauap_b": {"nDCG@10": [], "nDCG@20": [], "nDCG": []}}
+            for corr in corr_to_measure_to_vals.keys():
+                for qrel_file in qrel_files[t]:
+                    target_file = Path(f"../data/interim/{dataset}/qrels-analyzed/{qrel_file}/top-10-logo/reliability-test-results.json.gz")
+                    if not target_file.exists():
+                        skipped += 1
+                        print(target_file)
+                        continue
+                    with gzip.open(target_file, "rt") as f:
+                        i = json.loads(f.read())
+                        measures = {"nDCG@10": [], "nDCG@20": [], "nDCG": []}
+                        for r in i["system_ranking_evaluation"]:
+                            for m in measures.keys():
+                                measures[m].append(r[m][corr])
                         for m in measures.keys():
-                            measures[m].append(r[m][corr])
-                    for m in measures.keys():
-                        corr_to_measure_to_vals[corr][m].append(mean(measures[m]))
+                            corr_to_measure_to_vals[corr][m].append(mean(measures[m]))
  
-        if t == "full":
-            prefix = "\\cmark & \\cmark & \\cmark"
-        elif t == "title":
-            prefix = "\\cmark & \\xmark & \\xmark"
-        else:
-            raise ValueError("foo")
-        print(f'{prefix}  & {form("spearman", "nDCG@10")} & .xy & {form("spearman", "nDCG@20")} & .xy & {form("spearman", "nDCG")} & .xy & {form("tauap_b", "nDCG@10")} & .xy & {form("tauap_b", "nDCG@20")} & .xy & {form("tauap_b", "nDCG")} & .xy\\\\')
+
+
+
+        ret[dataset] = {
+             "nDCG@10": {
+                 "spearman": form("spearman", "nDCG@10"),
+                 "tauap_b": form("tauap_b", "nDCG@10"),
+             },
+             "nDCG@20": {
+                 "spearman": form("spearman", "nDCG@20"),
+                 "tauap_b": form("tauap_b", "nDCG@20"),
+             },
+             "nDCG": {
+                 "spearman": form("spearman", "nDCG"),
+                 "tauap_b": form("tauap_b", "nDCG"),
+             }
+        }
 
     print("skipped", skipped)
+
+    for field, prefix in [("title", "\\cmark & \\xmark & \\xmark"), ("title-description", "\\cmark & \\cmark & \\xmark"), ("title-narrative", "\\cmark & \\xmark & \\cmark"), ("full", "\\cmark & \\cmark & \\cmark")]:
+        line = [ret["dl19"]["nDCG@10"]["spearman"], ret["dl20"]["nDCG@10"]["spearman"]]
+        line += [ret["dl19"]["nDCG@20"]["spearman"], ret["dl20"]["nDCG@20"]["spearman"]]
+        line += [ret["dl19"]["nDCG"]["spearman"], ret["dl20"]["nDCG"]["spearman"]]
+        print(prefix + " & " + " & ".join(line) + "\\\\")
+
 
 if __name__ == '__main__':
     run_all_logo_tests()
